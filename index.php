8b3bf317a61b358dd2a7f443ffc641c43def9ef2
mhassan654 <hassansaava@gmail.com>
2022-04-16 22:53:53 +0300

    initial project commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$requestMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+$requestPath = $_SERVER['REQUEST_URI'] ?? '/';
+
+if($requestMethod === 'GET' and $requestPath === '/'){
+    print <<<HTML
+<!doctype html>
+<html lang="en">
+<body>
+hello saava
+</body>
+</html>
+HTML;
+}else if($requestPath === '/old-home'){
+//    header('Location: /', $replace = true, $code =301);
+    redirectForeverTo('/');
+    exit;
+    include(__DIR__.'/includes/404.php');
+}
