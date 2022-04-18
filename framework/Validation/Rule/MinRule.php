08e4a8b703ecb2567b974865db727208dc33ea89
mhassan654 <hassansaava@gmail.com>
2022-04-18 10:24:57 +0300

    added min rule validation

---
+++ framework/Validation/Rule/MinRule.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Framework\Validation\Rule;
+
+
+use InvalidArgumentException;
+
+class MinRule implements Rule
+{
+
+    public function validate(array $data, string $field, array $params)
+    {
+        // TODO: Implement validate() method.
+        if (empty($data[$field])):
+            return true;
+        endif;
+
+        if (empty($params[0])):
+            throw new InvalidArgumentException('specify a min length');
+            endif;
+
+        $length = (int) $params[0];
+        strlen($data[$field]) >= $length;
+    }
+
+    public function getMessage(array $data, string $field, array $params)
+    {
+        // TODO: Implement getMessage() method.
+        $length = (int) $params[0];
+        return "{$field} should be at least {$length} characters";
+    }
+}
