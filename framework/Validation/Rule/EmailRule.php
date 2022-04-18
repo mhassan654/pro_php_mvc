570472338f3245f02e14c9832fa39821971e5408
mhassan654 <hassansaava@gmail.com>
2022-04-18 10:19:19 +0300

    added Email validation rule

---
+++ framework/Validation/Rule/EmailRule.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Framework\Validation\Rule;
+
+
+class EmailRule implements Rule
+{
+
+    public function validate(array $data, string $field, array $params)
+    {
+        // TODO: Implement validate() method.
+        if (empty($data[$field])):
+            return true;
+            endif;
+        return str_contains($data[$field],'@');
+    }
+
+    public function getMessage(array $data, string $field, array $params)
+    {
+        // TODO: Implement getMessage() method.
+        return "{$field} should be an email";
+    }
+}
