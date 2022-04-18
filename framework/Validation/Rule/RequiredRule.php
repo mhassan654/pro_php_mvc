e5b1188ee59cbd7e41d78df25664bd7782b83fa4
mhassan654 <hassansaava@gmail.com>
2022-04-18 10:37:13 +0300

    added required validation rule

---
+++ framework/Validation/Rule/RequiredRule.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Framework\Validation\Rule;
+
+
+class RequiredRule implements Rule
+{
+
+    public function validate(array $data, string $field, array $params)
+    {
+        // TODO: Implement validate() method.
+        return !empty($data[$field]);
+    }
+
+    public function getMessage(array $data, string $field, array $params)
+    {
+        // TODO: Implement getMessage() method.
+        return "{$field} is required";
+    }
+}
